e21e9dac29c82eb93cb2b6c4b17e49e3fa429f49
samicrusader <hi@samicrusader.me>
2024-08-25 06:42:04 -0400

    feat: set path to paid `/usr/share/GeoIP/GeoIP2*.mmdb`

--- conf.php
+++ conf.php
@@ -5,13 +5,10 @@
 	$retrieveHost = true;
 	$retrieveComments = true;
 
-	$usePluginDatabase = true; // search and use GeoLite2 database in "database" plugin' directory
-
-	// If $usePluginDatabase is set to false:
-	$cityDbFile = "";	// empty = "/usr/share/GeoIP/GeoLite2-City.mmdb"
-	$countryDbFile = "";	// empty = "/usr/share/GeoIP/GeoLite2-Country.mmdb"
+	$usePluginDatabase = false;
+	$cityDbFile = "/usr/share/GeoIP/GeoIP2-City.mmdb";
+	$countryDbFile = "/usr/share/GeoIP/GeoIP2-Country.mmdb";
 
 	// For retrieve hosts
-
-	$dnsResolver = '1.1.1.1';	// use gethostbyaddr, if null
-	$dnsResolverTimeout = 1;	// timeout in seconds
+	$dnsResolver = '9.9.9.9';
+	$dnsResolverTimeout = 4;
